390b8ac6487ef85a61620139171a61a5b122f703
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-11-09 13:27:32 +0900

    [setting] 기본적인  폴더 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
+import Home from 'Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 
 const Router = () => {
-  return (
-    <div>
-      <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default Router;
